3e9123359ad8c06dadd5fb03d0ca19607cabf107
Gustavo Ribeiro <gustavors071998@gmail.com>
2018-12-19 09:20:19 -0200

    Lista com menu

--- teste.c
+++ teste.c
@@ -62,36 +62,91 @@ while(p != NULL && p->info != v){
     return l;
 }
 
-int main(){
+void liberaLista(Lista* l){
+Lista* p=l;
+while(p != NULL){
+    Lista* t=p->prox;
+    free(p);
+    p=t;
+    }
+}
 
+
+
+
+
+
+int main(){
 Lista* lista;
-lista = criaLista();
-if(vazia(lista)){
-   printf("Vazia!\n");
-}
-else{
-    printf("Cheia\n");
-}
-int a=0,b=0,d=0,e=0;
-printf("Quantos numeros quer inserir:");
+int op=0;
+
+int a=0,b=0,d=0,e=0,w=0;
+
+do{
+printf("\n98Escolha uma das opções abaixo:\n\n1-Criar a lista\n2-Inserir na lista\n3-imprimir lista\n4-Busca\n5-Remover da lista\n6-Liberar lista\n0-Sair\n\nOP:");
+scanf("%d",&op);
+system("cls || clear");
+switch (op)
+{
+case 0:
+    w=1;
+break;
+
+case 1:
+      lista = criaLista();
+      printf("\nCriada\n\n");
+break;
+
+case 2:
+      printf("Quantos numeros quer inserir:");
 scanf("%d",&a);
 for(int c=0;c<a;c++){
     printf("Numero %d: ",c+1);
     scanf("%d",&b);
 lista = insereLista(lista,b);
 }
-imprimiLista(lista);
-printf("Buscar: ");
+system("cls || clear");
+break;
+
+case 3:
+    system("cls || clear");
+    printf("Lista Impressa:\n\n");
+      imprimiLista(lista);
+      printf("\n");
+break;
+
+case 4:
+    system("cls || clear");
+      printf("Buscar: ");
 scanf("%d",&d);
 if(buscar(lista,d)==NULL){
-    printf("Não encontrou");
+    printf("\nNão encontrou\n\n");
     }
     else{
-        printf("Encontrou elemento");
+        printf("Encontrou elemento\n\n");
     }
+
+break;
+
+case 5:
+    system("cls || clear");
 printf("\nRemover: ");
 scanf("%d",&e);
 lista = removeLista(lista,e);
 printf("Depois da remoção:\n");
 imprimiLista(lista);
+
+break;
+
+case 6:
+    system("cls || clear");
+    liberaLista(lista);
+    printf("Liberada\n");
+break;
+default:
+    system("cls || clear");
+    printf("Opção nao e valida\n");
+    }
+}while(op);
+return 0;
 }
