e8fd9b86d21dec2d92face4338a228c12838a275
Gustavo Ribeiro <gustavors071998@gmail.com>
2018-12-21 11:34:56 -0200

    Lista Corrigida

--- teste.c
+++ teste.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
 
 typedef struct lista{
 int info;
-char nome[30];
+char nome[40];
 struct lista* prox;
 }Lista;
 
@@ -12,10 +12,10 @@ Lista* criaLista(){
     return NULL;
 }
 
-Lista* insereLista(Lista* l,int a,char n[30]){
+Lista* insereLista(Lista* l,int a,char *n){
     Lista* dados = (Lista*)malloc(sizeof(Lista));
     dados->info = a;
-    dados->nome[30] = n[30];
+    strcpy(dados->nome, n);
     dados->prox=l;
     return dados;
 }
@@ -23,7 +23,7 @@ Lista* insereLista(Lista* l,int a,char n[30]){
 void imprimiLista(Lista* l){
     Lista* p;
     for(p=l;p!=NULL;p=p->prox){
-        printf("%d\n%s",p->info,p->nome);
+        printf("%d\n%s\n\n",p->info,p->nome);
     }
 }
 
@@ -77,10 +77,10 @@ while(p != NULL){
 int main(){
 Lista* lista;
 int op=0;
-char n[30];
+char n[40];
 int a=0,b=0,d=0,e=0,w=0;
 do{
-printf("\nEscolha uma das opções abaixo:\n\n1-Criar a lista\n2-Inserir na lista\n3-imprimir lista\n4-Busca\n5-Remover da lista\n6-Liberar lista\n0-Sair\n\nOP:");
+printf("\nEscolha uma das opções abaixo:\n\n1-Criar a lista\n2-Inserir na lista\n3-Imprimir lista\n4-Busca\n5-Remover da lista\n6-Liberar lista\n0-Sair\n\nOP:");
 scanf("%d",&op);
 system("cls || clear");
 switch (op)
@@ -99,11 +99,11 @@ case 2:
     printf("Inserir:");
     scanf("%d",&a);
 for(int c=0;c<a;c++){
-    printf("Numero %d: ",(c+1));
-    scanf("%d",&b);
     printf("Nome %d:",(c+1));
     scanf("%s",n);
-lista = insereLista(lista,b,n);
+    printf("Numero %d: ",(c+1));
+    scanf("%d",&b);
+    lista = insereLista(lista,b,n);
 }
 system("cls || clear");
 
@@ -134,7 +134,7 @@ case 5:
 printf("\nRemover: ");
 scanf("%d",&e);
 lista = removeLista(lista,e);
-printf("Depois da remoção:\n");
+printf("\nDepois da remoção:\n\n");
 imprimiLista(lista);
 
 break;
@@ -149,5 +149,6 @@ default:
     printf("Opção nao e valida\n");
     }
 }while(op);
+
 return 0;
 }
